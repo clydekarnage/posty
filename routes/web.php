a560e9625ebab909fbbc832933e9a44ebe1e3762
clydekarnage <ibalezclyde@gmail.com>
2021-01-06 16:27:16 +0400

    registering users but there is bug in register view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
+// Register Route
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
 
 
 Route::get('/posts', function () {
